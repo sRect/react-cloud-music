04b6b3d909efb553210e42a36a553a7359823883
fangchaoqun <fcqun@sina.cn>
2020-03-26 15:24:27 +0800

    feat: :construction: 公共组件开发

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,5 +1,11 @@
 import React, { memo, Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
+import {
+  Top,
+  Tab,
+  TabItem,
+} from './style';
 
 const Home = (props) => {
   console.log(props)
@@ -7,7 +13,16 @@ const Home = (props) => {
 
   return (
     <Fragment>
-      <div>home</div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected"><TabItem><span > 推荐 </span></TabItem></NavLink>
+        <NavLink to="/singers" activeClassName="selected"><TabItem><span > 歌手 </span></TabItem></NavLink>
+        <NavLink to="/rank" activeClassName="selected"><TabItem><span > 排行榜 </span></TabItem></NavLink>
+      </Tab>
       {
         renderRoutes(routes)
       }
