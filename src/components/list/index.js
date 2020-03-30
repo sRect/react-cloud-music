476cc17c9cec740ac3a9c871b189cd610d645529
fangchaoqun <fcqun@sina.cn>
2020-03-30 15:06:46 +0800

    feat: :sparkles: react-lazyload图片懒加载

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,4 +1,5 @@
 import React, { memo } from 'react';
+import LazyLoad from 'react-lazyload';
 import { getCount } from "../../api/utils";
 import {
   ListWrapper,
@@ -19,8 +20,10 @@ const RecommendList = (props) => {
               <ListItem key={item.id + index}>
                 <div className="img_wrapper">
                   <div className="decorate"></div>
-                  {/* 加此参数可以减小请求的图片资源大小 */}
-                  <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music" />
+                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./music.png')} alt="music"/>}>
+                    {/* 加此参数可以减小请求的图片资源大小 */}
+                    <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music" />
+                  </LazyLoad>
                   <div className="play_count">
                     <i className="iconfont play">&#xe885;</i>
                     <span className="count">{getCount(item.playCount)}</span>
